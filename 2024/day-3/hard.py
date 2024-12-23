212912a56ab2cb32f96c436ee5ba44d4b1c48133
Chad Nierenhausen <chad.nierenhausen@gmail.com>
2024-12-22 22:47:26 -0700

    Some progress on part two of day 3. Still not quite done yet.

--- 2024/day-3/hard.py
+++ 2024/day-3/hard.py
@@ -16,7 +16,22 @@ with open(filename) as filep:
     p = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
     for line in filep:
         # Read file here
-        iterator = p.finditer(line)
+        chunks = line.strip().split("don't()")
+        valid = []
+        for i in range(len(chunks)):
+            if i == 0:
+                valid.append(chunks[i])
+                continue
+
+            start = chunks[i].find("do()")
+
+            if start != -1:
+                valid.append(chunks[i][start + 4:])
+
+        valid_line = ''.join(valid)
+        print(valid_line)
+
+        iterator = p.finditer(valid_line)
         for match in iterator:
             data.append([int(match.group(1)), int(match.group(2))])
 
